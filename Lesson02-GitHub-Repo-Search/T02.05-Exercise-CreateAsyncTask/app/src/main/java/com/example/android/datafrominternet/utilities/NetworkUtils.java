b85acb33d68b5e7ce85b05eba8c86ec08170eb6d
Greg Izzo <greg.izzo@gmail.com>
2018-01-21 14:52:30 -0500

    Updated with changes from edited lessons

--- Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ Lesson02-GitHub-Repo-Search/T02.05-Exercise-CreateAsyncTask/app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -88,4 +88,23 @@ public class NetworkUtils {
             urlConnection.disconnect();
         }
     }
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
 }
--- Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -17,13 +17,17 @@ package com.example.android.recyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
-
+    private static final int NUM_LIST_ITEMS = 100;
     // TODO (2) Create a GreenAdapter variable called mAdapter
+    GreenAdapter mAdapter;
     // TODO (3) Create a RecyclerView variable called mNumbersList
+    RecyclerView mNumbersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +35,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        // G: mNumbersList points to the 'number_list_item.xml' in res folder
+        mNumbersList = (RecyclerView) findViewById(R.id.rv_numbers);
 
         // TODO (5) Create a LinearLayoutManager variable called layoutManager
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+
         // TODO (6) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
+        // G: Created a layout manager and assigned it to our (RecyclerView) layout
+        mNumbersList.setLayoutManager(layoutManager);
 
         // TODO (7) Use setHasFixedSize(true) to designate that the contents of the RecyclerView won't change an item's size
+        mNumbersList.setHasFixedSize(true);
 
         // TODO (8) Store a new GreenAdapter in mAdapter and pass it NUM_LIST_ITEMS
+        // G: Creating an instance of our GreenAdapter, telling it how many items we will work with
+        mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
 
         // TODO (9) Set the GreenAdapter you created on mNumbersList
+        // Assign/connect the new GreenAdapter to our RecyclerView
+        mNumbersList.setAdapter(mAdapter);
     }
 }
